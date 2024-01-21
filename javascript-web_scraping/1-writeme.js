aabe6bdd3cb2232a0c8fb31a0b712a5b995104ab
ucflumm <ucflumm@gmail.com>
2024-01-22 10:24:10 +1100

    Add file writing functionality using fs.writeFile()

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const fs = require('fs');
+// argv[2] is the dest file name
+const file = process.argv[2];
+// argv[3] is the content to write
+const content = process.argv[3];
+
+fs.writeFile(file, content, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+});
