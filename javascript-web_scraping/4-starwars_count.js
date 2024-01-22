6482e3e83dd345e54ba48248e91c28ed82a79987
ucflumm <ucflumm@gmail.com>
2024-01-22 00:07:06 +0000

    Add web scraping functionality to count Star Wars characters

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    for (const film of films) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
+  }
+}
+);
