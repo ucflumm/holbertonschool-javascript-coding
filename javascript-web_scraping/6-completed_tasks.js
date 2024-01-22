7a87d4e661dfb43ef60472b02a467eed26bc17eb
ucflumm <ucflumm@gmail.com>
2024-01-22 00:10:40 +0000

    Add code to count completed tasks by user

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+const dict = {};
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+    for (const task of tasks) {
+      if (task.completed === true) {
+        if (dict[task.userId] === undefined) {
+          dict[task.userId] = 1;
+        } else {
+          dict[task.userId]++;
+        }
+      }
+    }
+    console.log(dict);
+  }
+}
+);
