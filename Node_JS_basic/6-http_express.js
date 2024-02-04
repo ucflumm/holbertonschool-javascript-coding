8d01fe0f75b3c7885ab9f37858d4eafa5c27131b
ucflumm <ucflumm@gmail.com>
2024-02-05 00:17:18 +1100

    added express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+}
+);
+
+app.listen(1245, () => {
+  console.log('Listening on port 1245');
+}
+);
+
+module.exports = app;
