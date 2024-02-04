5e83ea051f975cb5debd54d3089f9bad53339aa8
ucflumm <ucflumm@gmail.com>
2024-02-05 00:18:51 +1100

    Fix formatting in 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -4,12 +4,10 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
-}
-);
+});
 
 app.listen(1245, () => {
   console.log('Listening on port 1245');
-}
-);
+});
 
 module.exports = app;
