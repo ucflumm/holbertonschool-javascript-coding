25f1ec69688654a268f6435b4add776d9d259ddd
ucflumm <ucflumm@gmail.com>
2024-02-05 00:12:05 +1100

    Revert "Update HTTP server configuration"
    This reverts commit f9ce761e6061d52a6a6a1daeffb9ddfa3b17ad5f.

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,10 +3,5 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
-});
-
-app.listen(1245, () => {
-  console.log('Server is running on port 1245');
-});
-
-module.exports = app;
+}
+);
