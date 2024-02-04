e6dbf9681862fdf9e001a1f2d8d65a895efc0520
ucflumm <ucflumm@gmail.com>
2024-02-05 01:02:52 +1100

    Add readDatabase function to utils.js

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+function readDatabase(path) {
+  return new Promise((resolve, reject) =>
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) reject(Error('Cannot load the database'));
+      else resolve(data.toString().split('\n').filter((line) => line));
+    })
+  );
+}
