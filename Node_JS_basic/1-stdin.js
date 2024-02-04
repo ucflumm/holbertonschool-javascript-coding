cceb4dd44b0162ea7d8381d952066621adb6c7e5
ucflumm <ucflumm@gmail.com>
2024-02-04 22:45:23 +1100

    Refactor stdin.js to improve user interaction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,9 @@
-const name = "Welcome to Holberton School, what is your name?";
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-rl.question(name, (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
-}
-);
-rl.on('close', () => {
-  console.log('This important software is now closing');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  let name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+  process.stdout.write('This important software is now closing\n');
   process.exit();
 });
